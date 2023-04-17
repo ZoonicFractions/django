21c84a65dc26202ea04486e938bd9a93e8bfc1ea
MoisesAdame <moisesadame01@gmail.com>
2023-04-17 10:50:42 -0600

    reconfiguration of paths

--- gameServer/zoonicWebsite/static/js/findStudent.js
+++ gameServer/zoonicWebsite/static/js/findStudent.js
@@ -8,9 +8,9 @@ document.getElementById('button-search').addEventListener("click",() => {
         .then(response => response.json())
         .then(data => {
             if (data.status == 'success') {
-                location.href = `/dash/${difficultyInput}/${classInput.toUpperCase()}/${roleNumberInput}`
+                location.href = `/dashboard/${difficultyInput}/${classInput.toUpperCase()}/${roleNumberInput}`
             } else{
-                location.href = `/dash/notfound`
+                location.href = `/dashboard/notfound`
             }
         })
     }
